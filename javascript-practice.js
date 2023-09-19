56bbb651c1d58e21af178b3d1733c5ad5c9280f0
David <davefast2003@gmail.com>
2023-09-19 06:41:12 -0700

    first commit

---
+++ javascript-practice.js
@@ -0,0 +1,24 @@
+
+function isPrime(num){
+    var prime = true
+    if(num <= 1){
+        return ""
+    }else{
+        for(let i = 2; i < num; i++){
+            if(num % i == 0){
+                prime = false
+                break
+            }
+        }
+
+        if(prime){
+            return num + " is a prime number"
+        }else{
+            return ""
+        } 
+    }
+}
+
+for(i = 0; i < 30; i++){
+    console.log(isPrime(i))
+}
